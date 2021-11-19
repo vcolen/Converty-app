160be1e6b548137f2d897e586cc67dabd4d62898
VIctor <colen.victor@icloud.com>
2021-11-19 11:28:38 -0300

    Initial Commit

---
+++ Converty/ConvertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertyApp.swift
+//  Converty
+//
+//  Created by Victor Colen on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ConvertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
