e8747418f813024db27281ea6e6d70074a7d979d
VIctor <colen.victor@icloud.com>
2021-11-19 12:27:17 -0300

    organize data, add text field for input

--- Converty/ContentView.swift
+++ Converty/ContentView.swift
@@ -8,9 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    let measureTypes = ["Temperature", "Length", "Time", "Volume"]
+    let units = [
+        "Temperature" : [
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin"
+        ],
+        "Length" : [
+            "Meters",
+            "Kilometers",
+            "Feet",
+            "Yards",
+            "Miles"
+        ],
+        "Time" : [
+            "Seconds",
+            "Minutes",
+            "Hours",
+            "Days"
+        ],
+        "Volume" : [
+            "Milliliters",
+            "Liters",
+            "Cups",
+            "Pints",
+            "Gallons"
+        ]
+    ]
+    
+    @State private var chosenType = "Temperature"
+    
+    var chosenTypeMeasures: [String] {
+        return units[chosenType]!
+    }
+    
+    @State private var rootUnit = "Kelvin"
+    @State private var desiredUnit = "Celsius"
+    
+    @State private var inputMeasure = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Conversion type", selection: $chosenType) {
+                        ForEach(measureTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Type of measure you want to convert")
+                }
+                
+                Section {
+                    Picker("Unit", selection: $rootUnit) {
+                        ForEach(chosenTypeMeasures, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Unit you want to convert from")
+                }
+                
+                Section {
+                    Picker("Unit", selection: $desiredUnit) {
+                        ForEach(chosenTypeMeasures, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Unit you want to convert to")
+                }
+                
+                Section {
+                    TextField("Measure", value: $inputMeasure, format: .number
+                    )
+                        .keyboardType(.decimalPad)
+                }
+            }
+            .navigationTitle("Converty")
+        }
     }
 }
 
