3590a3d1a02736605ba866cad9285ab9267d88db
VIctor <colen.victor@icloud.com>
2021-11-20 13:18:25 -0300

    add meter conversion methods

--- Converty/Convert.swift
+++ Converty/Convert.swift
@@ -9,42 +9,63 @@ import Foundation
 
 struct Convert {
     
-    static func convert(_ input: Double, from firstType: String, to secondType: String) -> Double {
-        switch firstType {
-        case "Celsius":
-            switch secondType {
+    static func convert(_ input: Double, from firstType: String, to secondType: String, measureType: String) -> Double {
+        switch measureType {
+        case "Temperature":
+            switch firstType {
+            case "Celsius":
+                switch secondType {
+                case "Fahrenheit":
+                    return convertCelsiusToFahrenheit(input)
+                case "Kelvin":
+                    return convertCelsiusToKelvin(input)
+                default:
+                    return input
+                }
+                
             case "Fahrenheit":
-                return convertCelsiusToFahrenheit(input)
+                switch secondType {
+                case "Celsius":
+                    return convertFahrenheitToCelsius(input)
+                case "Kelvin":
+                    return convertFahrenheitToKelvin(input)
+                default:
+                    return input
+                }
+                
             case "Kelvin":
-                return convertCelsiusToKelvin(input)
+                switch secondType {
+                case "Celsius":
+                    return convertKelvinToCelsius(input)
+                case "Fahrenheit":
+                    return convertKelvinToFahrenheit(input)
+                default:
+                    return input
+                }
+                
             default:
                 return input
             }
             
-        case "Fahrenheit":
-            switch secondType {
-            case "Celsius":
-                return convertFahrenheitToCelsius(input)
-            case "Kelvin":
-                return convertFahrenheitToKelvin(input)
-            default:
-                return input
-            }
-            
-        case "Kelvin":
-            switch secondType {
-            case "Celsius":
-                return convertKelvinToCelsius(input)
-            case "Fahrenheit":
-                return convertKelvinToFahrenheit(input)
-            default:
-                return input
-            }
+            //        case "Length":
+            //            switch firstType {
+            //            case "Meters":
+            //
+            //                case "Kilometers"
+            //
+            //                case "Feet"
+            //
+            //                case "Yards"
+            //
+            //                case "Miles"
+            //            }
             
         default:
             return input
         }
     }
+    
+    //MARK: - Temperature Conversion
     
     static private func convertCelsiusToFahrenheit(_ value: Double) -> Double {
         value * 1.8 + 32
@@ -69,6 +90,27 @@ struct Convert {
     static private func convertKelvinToFahrenheit(_ value: Double) -> Double {
         (value - 273) * 1.8 + 32
     }
+    
+    //MARK: - Length Conversion
+
+    static private func convertMeterToKilometer(_ value: Double) -> Double {
+        value * 0.001
+    }
+    
+    static private func convertMeterToFeet(_ value: Double) -> Double {
+        value * 3.28084
+    }
+    
+    static private func convertMeterToYard(_ value: Double) -> Double {
+        value / 0.9144
+    }
+    
+    static private func convertMeterToMile(_ value: Double) -> Double {
+        value / 1_609.344
+    }
+    
+    
+    
 }
 
 
